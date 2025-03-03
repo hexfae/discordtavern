4ee4aa49c9631c96fecc86e2b8cf7f7f5adaa712
hexfae <hexfae@proton.me>
2025-03-03 23:15:04 +0100

    fix tts context command

--- src/commands/tts.rs
+++ src/commands/tts.rs
@@ -6,15 +6,33 @@ use crate::prelude::*;
 
 #[poise::command(context_menu_command = "Läs upp")]
 pub async fn läs_upp(ctx: Context<'_>, msg: serenity::Message) -> Result<()> {
-    let bytes = send_tts_request(msg.content).await?;
+    let content = if msg.content.is_empty() {
+        let Some(embed) = msg.embeds.first() else {
+            ctx.reply("Någonting gick fel här... #2").await?;
+            return Ok(());
+        };
+        let Some(ref content) = embed.description else {
+            ctx.reply("Någonting gick fel här... #2").await?;
+            return Ok(());
+        };
+        content.to_string()
+    } else {
+        msg.content.to_string()
+    };
+    let bytes = send_tts_request(content).await?;
     let attachment = CreateAttachment::bytes(bytes, format!("{}.mp3", ctx.id()));
     ctx.send(CreateReply::new().attachment(attachment)).await?;
     Ok(())
 }
 
-async fn send_tts_request(content: impl AsRef<str>) -> Result<Vec<u8>> {
+pub async fn send_tts_request(content: impl AsRef<str>) -> Result<Vec<u8>> {
     let response = reqwest::Client::new()
         .post("https://countik.com/api/text/speech")
+        .header(
+            "User-Agent",
+            "Mozilla/5.0 (X11; Linux x86_64; rv:134.0) Gecko/20100101 Firefox/134.0",
+        )
+        .header("Content-Type", "application/json")
         .body(format!(
             r#"{{"text": "{}", "voice": "en_us_001"}}"#,
             content.as_ref()
